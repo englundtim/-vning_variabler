d1a79595e5351305084131353fe89c25c98cc0a5
Tim <tim.englund@elev.ga.ntig.se>
2023-09-04 09:20:51 +0200

    Laggt till kod för heltalsoperationer

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int tal1 = 2;
+        int tal2 = 30;
+
+        int summa_addition = tal1+tal2;
+        int summa_subtraktion = tal1-tal2;
+
+        System.out.println("Summan av tal1 och tal2 = "+summa_addition);
+        System.out.println("Differensen mellan tal1 och tal2 = "+summa_subtraktion);
+
+        int produkt = tal1*tal2;
+        int kvot = tal1*tal2;
+
+        System.out.println("produkten av tal1 och tal2 = "+produkt);
+        System.out.println("Kvoten mellan tal1 och tal2 = "+kvot);
+
     }
 }
